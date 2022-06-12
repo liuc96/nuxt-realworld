2ac56af5c0530c07ed0e9d56f098bebb29991fc6
shizhu <liuc96@163.com>
2022-06-12 21:48:44 +0800

    release: v1.0.2

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">测试版本升级 1.0.1</h1>
+        <h1 class="logo-font">测试版本升级 1.0.2</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
